5cea3c4033afa62c5b01d96e3d72d6df4ed9505f
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-10-05 04:28:26 -0700

    Update TVShows link in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 	<h1>MTV Movies</h1>	
 
 	<div class="fuck">
-		<a href="http://10.0.4.54:8091/"><h3>TvShows</h3></a>
+		<a href="http://10.0.4.58:8091/"><h3>TvShows</h3></a>
 		<a href="http://10.0.4.39:9090/"><h3>Music</h3></a>
 	</div>
 	
