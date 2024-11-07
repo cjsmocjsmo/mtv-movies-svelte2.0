38fbd051ec30aab0286537f4e2cc7b14b8745666
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-11-06 21:06:19 -0800

    Fetch and display temperature and humidity data in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,21 @@
 	import  { onMount } from 'svelte';
 	
 
-	let temp = "";
+	let tempf = "";
+	let tempc = "";
+	let humi = "";
 	onMount(async () => {
     	const response = await fetch('http://10.0.4.60:8080/tempf');
     	const newData = await response.json();
-    	temp = newData;
-		console.log(temp);
+    	tempf = newData;
+
+		const response2 = await fetch('http://10.0.4.60:8080/tempc');
+		const newData2 = await response2.json();
+		tempc = newData2;
+
+		const response3 = await fetch('http://10.0.4.60:8080/humi');
+		const newData3 = await response3.json();
+		humi = newData3;
   	});
 </script>
 
@@ -24,7 +33,12 @@
 		<a href="http://10.0.4.58:8091/"><h3>TvShows</h3></a>
 		<a href="http://10.0.4.76:9090/"><h3>Music</h3></a>
 		<a href="/Search"><h3>Search</h3></a>
-		<h3>{temp}°F</h3>
+		
+	</div>
+	<div>
+		<h3>{tempf}°F</h3>
+		<h3>{tempc}°C</h3>
+		<h3>{humi}%H</h3>
 	</div>
 	
 	<div>
