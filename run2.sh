0e7f23e5833ca7e91069c40e4a92aabafe77d07d
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-24 03:28:05 -0700

    Refactor run2.sh to improve architecture handling and add support for Docker builds

--- run2.sh
+++ run2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 basepath=/home/pi/mtv-movies-svelte2.0;
+
 builddir=$basepath/build;
 
 cd $basepath;
@@ -8,18 +9,15 @@ git pull https://github.com/cjsmocjsmo/mtv-movies-svelte2.0.git;
 
 rm -rf $builddir;
 
+npm install;
+
 npm run build;
 
 arch=$(uname -m);
 
 if [ "$arch" = "armv7l" ]; then
-    echo "This archatecture is not supported"
-    exit 1
-fi
-
-if [ "$arch" = "i386" ]; then
-    echo "This archatecture is not supported"
-    exit 1
+    docker build -t mtvmovsvelte:arm32 -f ./arm32/Dockerfile .;
+    docker run --name mtvmovsvelt -d -p 8090:80 mtvmovsvelte:arm32;
 fi
 
 if [ "$arch" = "aarch64" ]; then
@@ -32,4 +30,7 @@ if [ "$arch" = "x86_64" ]; then
     docker run --name mtvmovsvelt -d -p 8090:80 mtvmovsvelte:amd64;
 fi
 
-
+if [ "$arch" = "i386" ]; then
+    echo "This archatecture is not supported"
+    exit 1
+fi
